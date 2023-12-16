0e5a83dc6af0084259a7fb2bc1365503385a7b48
Rohit <rohit.sharma9934@gmail.com>
2023-12-16 13:10:47 +0530

    Initial Commit

---
+++ Muralist/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Muralist
+//
+//  Created by Rohit Sharma on 16/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Muralist/MuralistApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuralistApp.swift
+//  Muralist
+//
+//  Created by Rohit Sharma on 16/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MuralistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
